13086d0e99e2c4008bad782f8ae301a82a1a8163
vivek-trueext <163299671+vivek-trueext@users.noreply.github.com>
2024-04-03 20:06:29 +0530

    landing page
    landing page

---
+++ Hosting Website landing page/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("body").tooltip({ selector: '[data-bs-toggle=tooltip]' });
+});
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
